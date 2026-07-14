8d2e9e3c72c7e5882286483f8415bda665fdf762
o0VanFanel0o <199848867+o0VanFanel0o@users.noreply.github.com>
2026-07-14 15:52:52 -0600

    Add react-icons dependency and enhance Footer component with social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import '../styles/Footer.css'
+import { FaInstagram, FaFacebookF, FaWhatsapp } from 'react-icons/fa'
 
 function Footer() {
     return (
@@ -12,6 +13,12 @@ function Footer() {
           <h3 className="footer__title">Contacto</h3>
           <p className="footer__whats">WhatsApp: (55) 1234 5678</p>
           <p className="footer__mail">Correo: contacto@hero.com</p>
+          <a href="https://www.instagram.com/tu_usuario" target="_blank" rel="noreferrer">
+            <FaInstagram />
+          </a>
+          <a href="https://www.facebook.com/tu_usuario" target="_blank" rel="noreferrer">
+            <FaFacebookF/>
+          </a>
         </div>
       </footer>
     )
