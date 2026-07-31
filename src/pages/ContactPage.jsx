ced1172b4944fb524afc2c724d204f11b7b0b66c
o0VanFanel0o <199848867+o0VanFanel0o@users.noreply.github.com>
2026-07-31 11:38:53 -0600

    Add contactPage style, adding features to ContactPage

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,3 +1,5 @@
+import "../styles/ContactPage.css"
+
 function ContactPage() {
 
     return(
@@ -7,10 +9,30 @@ function ContactPage() {
             Nos pondremos en contacto contigo para conocer más detalles.</p>
             <section className="contact-page__content">
                 <form action="">
-                    <label htmlFor="name" id="name" name="name">Nombre</label>
-                    <input type="text" />
-                    <label htmlFor="watsapp" id="watsapp" name="watsapp">Numero de WatsApp</label>
-                    <input type="" />
+                    <div className="contact-page__field">
+                        <label htmlFor="name">Nombre</label>
+                        <input type="text" id="name" name="name" autoComplete="name"required/>
+                    </div>
+                    <div className="contact-page__field">
+                        <label htmlFor="whatsapp">Numero de WhatsApp</label>
+                        <input type="tel" id="name" name="name" autoComplete="tel" required />
+                    </div>
+                    <div className="contact-page__field">
+                        <label htmlFor="email">Correo Electronico</label>
+                        <input type="email" id="email" name="email" autoComplete="email" />
+                    </div>
+                    <div>
+                        <label htmlFor="space">Selecciona el espacio</label>
+                        <select name="space" id="space" defaultValue="" required>
+                            <option value="" disabled>Espacio</option>
+                            <option value="recamara">Recamara</option>
+                            <option value="sala">Sala</option>
+                            <option value="cocina">Cocina</option>
+                            <option value="bano">Baño</option>
+                            <option value="comedor">Comedor</option>
+                            <option value="other">Otro</option>
+                        </select>
+                    </div>
                 </form>
             </section>
          </main>
