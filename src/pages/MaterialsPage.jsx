bc614ff82463bd6a091124e2e958ceecfbd3b60a
o0VanFanel0o <199848867+o0VanFanel0o@users.noreply.github.com>
2026-07-31 10:51:27 -0600

    add Contact and Material pages with the initial structure

---
+++ src/pages/MaterialsPage.jsx
@@ -0,0 +1,13 @@
+function MaterialPage() {
+
+    return(
+        <main className="material-page__main">
+            <h1 className='material-page__header'>Header</h1>
+            <div className="material-page__descripcion">
+                <p>Descripcion</p>
+            </div>
+        </main>
+    )
+}
+
+export default MaterialPage
