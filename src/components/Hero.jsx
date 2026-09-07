ec5b85cd2d7010dae274fe59b02d5df5182b6958
o0VanFanel0o <199848867+o0VanFanel0o@users.noreply.github.com>
2026-09-07 12:12:55 -0600

    Update Hero component with background image and adjust styles for improved layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,12 @@
 function Hero() {
     return (
       <section className="hero">
-        <div className="hero__image">
+        <div className="hero__image" style={{ 
+          backgroundImage: "url('/catalogo-muebles/BD-1/separador-001/img-1.jpg')" }}>
             <div className="hero__content">
                 <h1 className="hero__brand">Prodima</h1>
                 <h2>Muebles a medida para cada espacio de su hogar</h2>
                 <p>Diseñamos y fabricamos piezas personalizadas según sus necesidades.</p>
-                <button className="hero__button" type="button">
-                    Ver catálogo
-                </button>
             </div>
         </div>
       </section>
